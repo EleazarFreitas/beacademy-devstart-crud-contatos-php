33fc58c4d073b97303af70d69e0038ae97ebdd6c
EleazarSF <eleazarsf@gmail.com>
2022-06-03 14:33:27 -0300

    crud - agenda de contatos - finalizada

---
+++ pages/404.php
@@ -0,0 +1,3 @@
+<section>
+    <img class="img-fluid" src="https://colorlib.com/cdn-cgi/image/width=1920,height=933,fit=crop,quality=80,format=auto,onerror=redirect,metadata=none/wp-content/uploads/sites/2/404-error-template-3.png">
+</section>
---
+++ pages/home.php
@@ -0,0 +1,10 @@
+<section>
+    <h1>DevStart</h1>
+    <hr>
+    <h2>#PayLivre #BeAcademy</h2>
+</section>
+<style>
+    section {
+        color: #212529;
+    }
+</style>
